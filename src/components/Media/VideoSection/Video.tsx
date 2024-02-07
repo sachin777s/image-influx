2ec5c9072f33bc7306d714bf255e9eae2642f399
Sachin Kumar <stdtech7878@gmail.com>
2024-02-07 17:49:47 +0530

    Media Videos Section is created

--- src/components/Media/VideoSection/Video.tsx
+++ src/components/Media/VideoSection/Video.tsx
@@ -1,7 +1,44 @@
+import { Button } from '@nextui-org/button'
+import Link from 'next/link'
 import React from 'react'
+import { FaDownload, FaRegHeart } from 'react-icons/fa'
+import { MdOutlineCollectionsBookmark } from 'react-icons/md'
 
-export default function Video() {
+export default function Video(props: {
+  isControls: boolean;
+  title: string;
+  src:string
+}) {
+
+  const { isControls, title, src } = props;
+
   return (
-    <div>Video</div>
+    <div className="group relative overflow-hidden">
+      <div className='w-full p-2 absolute top-0 left-0 z-[11] opacity-0 transition duration-500 group-hover:opacity-100 flex items-center gap-2'>
+        <span className="absolute top-0 w-full h-1 bg-transparent -translate-y-full shadow-[0_0_37px_24px_black] z-0"></span>
+        <Button isIconOnly className='bg-white'>
+          <MdOutlineCollectionsBookmark color='var(--secondary-text)' size={24} />
+        </Button>
+        <Button isIconOnly className='bg-white'>
+          <FaRegHeart color='var(--secondary-text)' size={24} />
+        </Button>
+      </div>
+      <video
+        className=""
+        width={900}
+        autoPlay
+      >
+        <source src={src} />
+      </video>
+      <div className='w-full px-2 py-1 absolute left-0 bottom-0 z-10 opacity-0 transition duration-500 group-hover:opacity-100 flex items-center justify-between overflow-hidden'>
+        <span className="absolute left-0 bottom-0 w-full h-1 bg-transparent translate-y-full shadow-[0_0_37px_24px_black] z-0"></span>
+        <p className="text-white z-10">{title}</p>
+        <Link href="/">
+          <Button className='bg-[var(--primary-color)] text-white'>
+            Dowload <FaDownload />
+          </Button>
+        </Link>
+      </div>
+    </div>
   )
 }
--- src/components/Media/VideoSection/VideosTab.tsx
+++ src/components/Media/VideoSection/VideosTab.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Video from './Video'
 
 export default function VideosTab() {
   return (
-    <div>VideosTab</div>
+    <div className='w-full columns-4 gap-4 space-y-4'>
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/904304263/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=92aba6d264ec10a5e5e14ffa90b89ed630e03017dfaaf51311261473fcda969d" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/external/458348199.sd.mp4?s=6dc852919a616691b3ae1a63f569c0dd7efb59a2&profile_id=164&oauth2_token_id=57447761" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/796129964/rendition/540p/file.mp4?loc=external&oauth2_token_id=57447761&signature=437f57d272d4a3919052075fdfce216e403bd65c743b1b700d66da9c27982585" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/905571979/rendition/360p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=fad264e8d73a234ad8d9c820b18b2c8ab60d9928009abb10125fe4bb034f1508" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/external/477953727.sd.mp4?s=882d3f367bb375bdf824324450380c7445613180&profile_id=164&oauth2_token_id=57447761" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/883455384/rendition/360p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=707afb0d802486a70764b132d0b59b73eccd56114dd24ad76c51a77811dee423" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/784260807/rendition/540p/file.mp4?loc=external&oauth2_token_id=57447761&signature=640687af575374caf5a0e20506cfc6d98957c3e016c14c666a7354c0be01a214" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/889676806/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=4fd8268624c7f978517e35bde8f7a6f1b796a37437ae25257cc23b7aee016378" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/883528090/rendition/360p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=139bc0b13e6eee9c4207f5cbd86b844606430651da51f68294af39fad7850b4c" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/external/499163955.sd.mp4?s=0c89bc5df95d06724809fc2161e8310966b0eb45&profile_id=165&oauth2_token_id=57447761" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/891643300/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=eafaf8fbf65e988303fc860a09a252e0a89a35c990b1c12cfcd9e85df7949cee" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/884559250/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=035134f69f149ce6e8f71d971f13bf47cfa560cc912e63cc8df5e464ad5de117" title="Nature video shot" isControls />
+      <Video src="https://player.vimeo.com/progressive_redirect/playback/884816874/rendition/540p/file.mp4?loc=external&oauth2_token_id=1747418641&signature=b487e45b6e030d93c1c0aba38926659cbd158990754cd2610db2bbd6ba3ac284" title="Nature video shot" isControls />
+    </div>
   )
 }
